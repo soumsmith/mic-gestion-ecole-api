6a72ffb9b5a0b03363506dac4af97d2c11e9aa5e
stephane <steph61088@gmail.com>
2024-01-31 12:46:36 +0000

    correction service suite

--- src/main/java/com/vieecoles/steph/entities/AppelNumerique.java
+++ src/main/java/com/vieecoles/steph/entities/AppelNumerique.java
@@ -4,9 +4,11 @@ import java.util.Date;
 import java.util.UUID;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -17,6 +19,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 @Table(name = "appel_numerique")
 public class AppelNumerique extends PanacheEntityBase{
 	
@@ -30,8 +33,14 @@ public class AppelNumerique extends PanacheEntityBase{
 	private String heureDebut;
 	@Column(name = "heure_fin")
 	private String heureFin;
+	@ManyToOne
+	@JoinColumn(name = "seance_id")
 	private Seances seance;
+	@JoinColumn(name = "personnel_id")
+	@ManyToOne
 	private Personnel personnel;
+	@JoinColumn(name = "ecole_id")
+	@ManyToOne
 	private Ecole ecole;
 	@Column(name = "progression_id")
 	private String progression;
