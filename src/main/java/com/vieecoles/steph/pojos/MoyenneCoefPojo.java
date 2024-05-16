9f28a76a0a1faa56f3f1d86ecbfb519e27c48653
Stéphane N'DRI <steph61088@gmail.com>
2024-05-16 20:31:02 +0000

    fix moyenne français et coef

---
+++ src/main/java/com/vieecoles/steph/pojos/MoyenneCoefPojo.java
@@ -0,0 +1,14 @@
+package com.vieecoles.steph.pojos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MoyenneCoefPojo {
+	private Long matiereId;
+	private String matiere;
+	private Double moyenne;
+	private Double coef;
+
+}
