37d178a0741d36daf0a69bf31f1a7920386e1577
soumsmith <soumsmith1@gmail.com>
2026-01-15 00:14:05 +0000

    nouveau rapport cio

---
+++ src/main/java/com/vieecoles/ressource/operations/etats/CIOModeleDeuxRessource.java
@@ -0,0 +1,125 @@
+package com.vieecoles.ressource.operations.etats;
+
+
+import com.vieecoles.dto.*;
+import com.vieecoles.entities.operations.ecole;
+import com.vieecoles.services.etats.*;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsReportConfiguration;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.*;
+
+@Path("/imprimer-rapport-cio-modele-deux")
+//@Produces(MediaType.APPLICATION_JSON)
+//@Consumes(MediaType.APPLICATION_JSON)
+
+public class CIOModeleDeuxRessource {
+    @Inject
+    EntityManager em;
+    @Inject
+    DpspServices dpspServices ;
+    @Inject
+    @ConfigProperty(name = "USER")
+    private String USER ;
+    @Inject
+    @ConfigProperty(name = "PASS")
+    private String PASS ;
+
+
+    private static String UPLOAD_DIR = "/data/";
+
+
+    @GET
+    @Transactional
+    @Path("/pouls-rapport-cio-modele-deux/{idEcole}/{libellePeriode}/{libelleAnnee}")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public ResponseEntity<byte[]>  getDtoRapport(@PathParam("idEcole") Long idEcole ,@PathParam("libellePeriode") String libellePeriode ,
+                                                 @PathParam("libelleAnnee") String libelleAnnee) throws Exception, JRException {
+        InputStream myInpuStream ;
+        /*myInpuStream = this.getClass().getClassLoader().getResourceAsStream("etats/BulletinBean.jrxml");*/
+        myInpuStream = this.getClass().getClassLoader().getResourceAsStream("etats/spider/CIO_MODELE_2.jrxml");
+        
+        spiderDspsDto detailsBull= new spiderDspsDto() ;
+        List<DspsDto>  dspsDto = new ArrayList<>() ;
+
+       /* try {
+            dspsDto = dpspServices.DspspDto(idEcole,libellePeriode,libelleAnnee) ;
+
+        } catch (RuntimeException e){
+            e.printStackTrace ();
+        }*/
+
+
+
+       // detailsBull.setDspsDto(dspsDto);
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://db:3306/ecoleviedbv2", USER, PASS);
+        //Connection connection = DriverManager.getConnection("jdbc:mysql://94.23.162.171:33061/ecoleviedbv2", USER, PASS);
+        JasperReport compileReport = JasperCompileManager.compileReport(myInpuStream);
+
+        //   JasperReport compileReport = (JasperReport) JRLoader.loadObjectFromFile(UPLOAD_DIR+"BulletinBean.jasper");
+       Locale.setDefault(Locale.US);
+       Locale franceLoc = new Locale("en", "US");
+       
+        Map<String, Object> map = new HashMap<>();
+        map.put("idEcole", idEcole);
+        map.put("annee", libelleAnnee);
+        map.put("periode", libellePeriode);
+        // map.put("title", type);
+          try {
+            JasperPrint report = JasperFillManager.fillReport(compileReport, map, connection);
+
+        } catch (RuntimeException e){
+            e.printStackTrace ();
+        }
+        JasperPrint report = JasperFillManager.fillReport(compileReport, map, connection);
+        JRXlsExporter exporter = new JRXlsExporter();
+        exporter.setExporterInput(new SimpleExporterInput(report));
+        
+        SimpleXlsReportConfiguration config = new SimpleXlsReportConfiguration();
+        config.setDetectCellType(true);
+        
+// 👈 Tout en texte
+       //exporter.setConfiguration(config);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(baos));
+        
+        exporter.exportReport();
+        
+        byte[] data = baos.toByteArray() ;
+        HttpHeaders headers= new HttpHeaders();
+        // headers.set(HttpHeaders.CONTENT_DISPOSITION,"inline;filename=Rapport"+myScole.getEcoleclibelle()+".docx");
+        headers.set(HttpHeaders.CONTENT_DISPOSITION,"inline;filename=Rapport Pouls-Scolaire-cio-modele-deux.xls");
+        return ResponseEntity.ok().headers(headers).contentType(org.springframework.http.MediaType.MULTIPART_FORM_DATA).body(data);
+
+
+
+    }
+
+
+
+
+
+}
